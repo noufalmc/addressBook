907e896ef2c1a3ac841537f3a137d28937e9a9c4
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-21 18:30:59 +0530

    Added:Welcome message

---
+++ src/corejava/addressBookMainClass.java
@@ -0,0 +1,14 @@
+/*
+Author : Noufal MC
+Date   : 20/01/2022
+Address Book Using Array List
+* */
+
+package corejava;
+
+public class addressBookMainClass {
+    public static  void main(String args[])
+    {
+        System.out.println("Welcome To Address Book!!!");
+    }
+}
