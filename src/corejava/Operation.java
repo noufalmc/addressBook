54b4b3dc9f83af4e1de9acd6c65d981f620c55e7
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-21 23:55:32 +0530

    added:hash map

--- src/corejava/Operation.java
+++ src/corejava/Operation.java
@@ -7,8 +7,8 @@ package corejava;
 import java.util.*;
 public class Operation
 {
-    //instance variable declaration
-    public static ArrayList<Person> addressBook=new ArrayList<>();
+    //instance variable declaration Hash map
+    public static HashMap<String,Person> addressBook=new HashMap();
     Scanner sc=new Scanner(System.in);
     String lastName,address,email,city,mobile,firstName,state;
     int zip;
@@ -18,25 +18,31 @@ public class Operation
     * */
     public  void addContacts()
     {
-        sc.nextLine();
-        System.out.println("Enter the FirstName");
-        firstName=sc.nextLine();
-        System.out.println("Enter the LastName");
-        lastName=sc.nextLine();
-        System.out.println("Enter the Address");
-        address=sc.nextLine();
-        System.out.println("Enter the City");
-        city=sc.nextLine();
-        System.out.println("Enter the State");
-        state=sc.nextLine();
-        System.out.println("Enter the Email");
-        email=sc.nextLine();
-        System.out.println("Enter the Mobile");
-        mobile=sc.nextLine();
-        System.out.println("Enter the Pincode");
-        zip=sc.nextInt();
-        Person p=new Person(firstName,lastName,address,email,city,state,mobile,zip);
-        addressBook.add(p);
+                sc.nextLine();
+                System.out.println("Enter the FirstName");
+                firstName = sc.nextLine();
+            if(checkPresntOrNot(firstName)) {
+                System.out.println("Enter the LastName");
+                lastName = sc.nextLine();
+                System.out.println("Enter the Address");
+                address = sc.nextLine();
+                System.out.println("Enter the City");
+                city = sc.nextLine();
+                System.out.println("Enter the State");
+                state = sc.nextLine();
+                System.out.println("Enter the Email");
+                email = sc.nextLine();
+                System.out.println("Enter the Mobile");
+                mobile = sc.nextLine();
+                System.out.println("Enter the Pincode");
+                zip = sc.nextInt();
+                Person p = new Person(firstName, lastName, address, email, city, state, mobile, zip);
+                addressBook.put(firstName, p);
+            }
+            else
+            {
+                System.out.println("Contact is Already exist!!!");
+            }
     }
     /**
      * method to update contact in Address Book
@@ -49,8 +55,10 @@ public class Operation
     public void editContacts()
     {
 
-        int status=contactIsPresent();
-        if(status!=0)
+        sc.nextLine();
+        System.out.println("Enter the first name");
+        String name=sc.nextLine();
+        if(!checkPresntOrNot(name))
         {
             System.out.println("Which fields want to be edit");
             System.out.println("[1] First Name");
@@ -68,7 +76,7 @@ public class Operation
                     System.out.println("Enter the FirstName");
                     firstName=sc.nextLine();
                     System.out.println("UPADTED FIRST NAME"+firstName);
-                    addressBook.get(status).setFirstName(firstName);
+                    addressBook.get(name).setFirstName(firstName);
                     break;
 
                 case 2:
@@ -76,41 +84,41 @@ public class Operation
                     System.out.println("Enter the lastName");
                     lastName=sc.nextLine();
                     System.out.println("UPADTED LAST NAME "+lastName);
-                    addressBook.get(status).setLastName(lastName);
+                    addressBook.get(name).setLastName(lastName);
                     break;
                 case 3:
                     sc.nextLine();
                     System.out.println("Enter the Address");
                     address=sc.nextLine();
                     System.out.println("UPADTED Adress"+address);
-                    addressBook.get(status).setAddress(address);
+                    addressBook.get(name).setAddress(address);
                     break;
                 case 4:
                     sc.nextLine();
                     System.out.println("Enter the City");
                     city=sc.nextLine();
-                    addressBook.get(status).setCity(city);
+                    addressBook.get(name).setCity(city);
                     System.out.println("UPADTED CITY "+city);
                     break;
                 case 5:
                     sc.nextLine();
                     System.out.println("Enter the Email");
                     email=sc.nextLine();
-                    addressBook.get(status).setEmail(email);
+                    addressBook.get(name).setEmail(email);
                     System.out.println("UPADTED EMAIL "+email);
                     break;
                 case 6:
                     sc.nextLine();
                     System.out.println("Enter the Mobile");
                     mobile=sc.nextLine();
-                    addressBook.get(status).setPhoneNumber(mobile);
+                    addressBook.get(name).setPhoneNumber(mobile);
                     System.out.println("UPADTED MOBILE "+mobile);
                     break;
                 case 7:
                     sc.nextLine();
                     System.out.println("Enter the Pincode");
                     zip=sc.nextInt();
-                    addressBook.get(status).setZip(zip);
+                    addressBook.get(name).setZip(zip);
                     System.out.println("UPADTED PINCODE "+zip);
             }
         }
@@ -119,25 +127,8 @@ public class Operation
             System.out.println("Contact Not Found");
         }
     }
-    /**
-     * method to show the contact is  present or not
-     * if contact is present returns the index value
-     */
-    public int contactIsPresent()
-    {
-        String firstName;
-        sc.nextLine();
-        System.out.println("Enter The First Name");
-        firstName=sc.nextLine();
-        for(int i=0;i<addressBook.size();i++) {
-            if (addressBook.get(i).getFirstName().equals(firstName)) {
-                return i;
-            }
-        }
-        return 0;
 
 
-    }
     /**
      * method is used to remove contact from address book
      * user can searh their name by using firstname
@@ -145,10 +136,12 @@ public class Operation
     * */
     public void deleteContacts()
     {
-        int status=contactIsPresent();
-        if(status!=0)
+        sc.nextLine();
+        System.out.println("Enter the first name");
+        String name=sc.nextLine();
+        if(!checkPresntOrNot(name))
         {
-            addressBook.remove(status);
+            addressBook.remove(name);
             System.out.println("Item Removed");
         }
         else
@@ -163,20 +156,38 @@ public class Operation
      */
         public void display()
         {
-            for(int i=0;i<addressBook.size();i++)
+            if(addressBook.size()>0) {
+                for (Person key : addressBook.values()) {
+                    System.out.println("Fisrt Name=" + key.getFirstName() + "\n");
+                    System.out.println("last Name=" + key.getLastName() + "\n");
+                    System.out.println("Address=" + key.getAddress() + "\n");
+                    System.out.println("City=" + key.getCity() + "\n");
+                    System.out.println("State=" + key.getState() + "\n");
+                    System.out.println("Mobile=" + key.getPhoneNumber() + "\n");
+                    System.out.println("email=" + key.getEmail() + "\n");
+                    System.out.println("Zip=" + key.getZip() + "\n");
+                }
+            }
+            else
             {
+                System.out.println("No data Found");
+            }
 
-                System.out.println("First Name " + addressBook.get(i).getFirstName());
-                System.out.println("Last Name " + addressBook.get(i).getLastName());
-                System.out.println("Address " + addressBook.get(i).getAddress());
-                System.out.println("City " + addressBook.get(i).getCity());
-                System.out.println("Email " + addressBook.get(i).getEmail());
-                System.out.println("Pincode " +addressBook.get(i).getZip());
-            }
-            if(addressBook.isEmpty())
-            {
-                System.out.println("The List Is Empty");
-            }
         }
-
+    /**
+     * method check the given contact exist or not
+     * if present it will return true
+     * otherwise return false
+     * */
+    public  boolean checkPresntOrNot(String firstName)
+    {
+        if(addressBook.containsKey(firstName))
+        {
+            return  false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
