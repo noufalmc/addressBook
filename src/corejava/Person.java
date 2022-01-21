cc0092ecaabe453e3c807059f5fed5b96e48e338
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-21 18:55:51 +0530

    UC1:created structure of the address book

---
+++ src/corejava/Person.java
@@ -0,0 +1,79 @@
+package corejava;
+/*
+Author : Noufal MC
+Date   : 20/01/2022
+
+* */
+public class Person {
+    /*
+    * Declaring Variable Required for Address Book
+    * Used getter and setter
+    * */
+    private static String firstName,lastName,address,city,state,phoneNumber;
+    private static int zip;
+    Person(String firstName,String lastName,String address,String city,String state,String phoneNumber)
+    {
+       this.firstName=firstName;
+       this.lastName=lastName;
+       this.address=address;
+       this.city=city;
+       this.state=state;
+       this.phoneNumber=phoneNumber;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName=firstName;
+    }
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+    public void setLastName(String lastName)
+    {
+        this.lastName=lastName;
+    }
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+    public void setCity(String city)
+    {
+        this.city=city;
+    }
+    public String city()
+    {
+        return this.city;
+    }
+    public void setZip(int zip)
+    {
+        this.zip=zip;
+    }
+    public int getZip()
+    {
+        return this.zip;
+    }
+    public void setAddress(String address)
+    {
+        this.address=address;
+    }
+    public String getAddress()
+    {
+        return this.address;
+    }
+    public void setState(String state)
+    {
+        this.state=state;
+    }
+    public String getState()
+    {
+        return this.state;
+    }
+    public void  setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber=phoneNumber;
+    }
+    public String getPhoneNumber()
+    {
+        return this.phoneNumber;
+    }
+}
